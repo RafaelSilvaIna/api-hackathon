72d0be6bf494d5fcd7ff3bc86cb593c1d378533d
Rafael Silva Inacio <rafaelsilvainaciobr@gmail.com>
2024-11-22 11:54:20 -0300

    Update PortalhackathonApplicationTests.java

--- src/test/java/br/com/ufc/portalhackathon/PortalhackathonApplicationTests.java
+++ src/test/java/br/com/ufc/portalhackathon/PortalhackathonApplicationTests.java
@@ -1,16 +1,14 @@
 package br.com.ufc.portalhackathon;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PortalhackathonApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void shouldLoadApplicationContext() {
+        
     }
 
 }
